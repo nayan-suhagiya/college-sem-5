e2291ba9601695f11d557ae32013c092f0e5395d
utsavp <utsavparmar72@gmail.com>
2023-07-29 02:08:11 +0530

    userinfo and category page changes

--- admin/userInfo.php
+++ admin/userInfo.php
@@ -1,3 +1,38 @@
+<?php
+include_once "../connection.php";
+
+if (isset($_POST["submit"]) && isset($_POST["name"]) && isset($_POST["user_id"]) && isset($_POST["email"]) && isset($_POST["user_type"])) {
+
+    $user_id = $_POST["user_id"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $user_type = $_POST["user_type"];
+    echo ("update  Users name = '$name' , email = '$email' , user_type = '$user_type'  where user_id = '$user_id'");
+    $query = "update  Users set  name = '$name' , email = '$email' , user_type = '$user_type'  where user_id = '$user_id'";
+    $runquery = mysqli_query($conn, $query);
+    if ($runquery) {
+        echo "
+        <script>
+          alert('User Update successfully!');
+        </script>
+        ";
+    }
+}
+if (isset($_POST["delete"]) && isset($_POST["user_id"])) {
+    $user_id = $_POST["user_id"];
+    echo("delete from Users  where user_id = '$user_id'");
+    $query = "delete from Users  where user_id = '$user_id'";
+    $runquery = mysqli_query($conn, $query);
+    if ($runquery) {
+        echo "
+        <script>
+          alert('User delete successfully!');
+        </script>
+        ";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,69 +67,80 @@
                             </thead>
 
                             <tbody>
-                                <?php $query = "SELECT * FROM Users";
+                                <?php
+                                $query = "SELECT * FROM Users";
                                 $result = mysqli_query($conn, $query);
                                 $i = 1;
                                 while ($row = mysqli_fetch_assoc($result)) {
-                                    echo "<tr>
-                                                <th scope='row'>$i</th>
-                                                <td>$row[name]</td>
-                                                <td>$row[email]</td>
-                                                <td>$row[user_type]</td>
-                                                <td>$row[image]</td>
-                                                <td>
-                                                <button class='btn btn-primary' data-bs-toggle='modal' onclick='modalOpen()' data-bs-target='#edit-employee-modal'><i class='bi bi-pencil'></i></button>
-                                                <button class='btn btn-danger'><i class='bi bi-trash'></i></button>
-                                                </td>
-                                                </tr>";
+                                ?>
+                                    <tr>
+                                        <th scope='row'><?= $i ?></th>
+                                        <td><?= $row["name"] ?></td>
+                                        <td><?= $row["email"] ?></td>
+                                        <td><?= $row["user_type"] ?></td>
+                                        <td><?= $row["image"] ?></td>
+                                        <td>
+                                            <form method="post" id='edit-form'>
+                                            <input class='form-control' type='hidden' value="<?= $row["user_id"] ?>" name='user_id'>
+
+                                                <button class='btn btn-primary' type="button" data-bs-toggle='modal' data-bs-target='#edit-employee-modal<?= $i ?>'><i class='bi bi-pencil'></i></button>
+                                                <button class='btn btn-danger' type="submit" name="delete"><i class='bi bi-trash'></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    <div class='modal fade' id='edit-employee-modal<?= $i ?>' tabindex='-1' style='display: none;' aria-hidden='true'>
+                                        <div class='modal-dialog modal-dialog-centered'>
+                                            <div class='modal-content'>
+                                                <div class='modal-header'>
+                                                    <h5 class='modal-title'>Update User()</h5>
+                                                    <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                                                </div>
+                                                <form method="post" id='edit-form'>
+                                                    <div class='modal-body'>
+                                                        <input class='form-control' type='hidden' value="<?= $row["user_id"] ?>" name='user_id'>
+                                                        <div class='form-group'>
+                                                            <label for='email'>Email</label>
+                                                            <input class='form-control' value="<?= $row["email"] ?>" type='text' name='email'>
+                                                        </div>
+                                                        <div class='form-group'>
+                                                            <label for='full_name'>Full Name</label>
+                                                            <input class='form-control' value="<?= $row["name"] ?>" type='text' name='name'>
+                                                        </div>
+                                                        <div class='form-group'>
+                                                            <label for='user_type'>User Type</label>
+                                                            <input class='form-control' value="<?= $row["user_type"] ?>" type='text' name='user_type'>
+                                                        </div>
+
+
+                                                    </div>
+                                                    <div class='modal-footer'>
+                                                        <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+                                                        <button type='submit' name="submit" class='btn btn-primary'>Save changes</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
                                     $i++;
                                 }
-
                                 ?>
+
                             </tbody>
                         </table>
                         <!-- End Table with stripped rows -->
                     </div>
                 </div> <!-- Left side columns -->
             </div>
-            <div class="modal fade" id="edit-employee-modal" tabindex="-1" style="display: none;" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Update User()</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <form action="update.php" id="edit-form">
-                            <div class="modal-body">
-                                <input class="form-control" type="hidden" name="id">
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input class="form-control" type="text" name="email">
-                                </div>
-                                <div class="form-group">
-                                    <label for="full_name">Full Name</label>
-                                    <input class="form-control" type="text" name="full_name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="user_type">User Type</label>
-                                    <input class="form-control" type="text" name="user_type">
-                                </div>
 
-
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
         </section>
     </main><!-- End #main -->
 </body>
 <?php
+function demo()
+{
+}
+?>
 
-    ?>
 
 </html>
